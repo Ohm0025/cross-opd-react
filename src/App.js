047b288ee8123e238d701e9a31ac04a69dc09dc4
Ohm0025 <toyohm0025@gmail.com>
2023-07-16 17:07:03 +0700

    draft registerPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/loginPage/LoginPage";
+import RegisterPage from "./pages/registerPage/RegisterPage";
+import ExamPage from "./pages/examPage/ExamPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<LoginPage />} path="/"></Route>
+      <Route element={<RegisterPage />} path="/register"></Route>
+      <Route element={<ExamPage />} path="/exam"></Route>
     </Routes>
   );
 }
---
+++ src/pages/examPage/ExamPage.js
@@ -0,0 +1,10 @@
+function ExamPage() {
+  return (
+    <div>
+      <div className="exam-header"></div>
+      <div className="exam-container"></div>
+    </div>
+  );
+}
+
+export default ExamPage;
